666779c515ec9bc1ccee0d3d281ea677ca83210a
ZakyKh <mzakykhairuddin@gmail.com>
2023-08-09 20:29:56 +0700

    add basic math program

---
+++ math.php
@@ -0,0 +1,14 @@
+<?php
+$x = 10;
+$y = 3;
+echo $x + $y . "\n";
+echo $x - $y . "\n";
+echo intdiv($x,$y) . "\n";
+echo $x / $y . "\n";
+echo $x * $y . "\n";
+echo $x % $y . "\n";
+
+$z = $x + $y;
+
+echo "$x $y $z\n";
+?>
